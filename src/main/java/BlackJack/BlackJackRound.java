8a9613b9770b19966126b3c4b61b2749225df695
Tomás Vitorino <up202405342@up.pt>
2026-01-01 15:57:54 +0000

    I have done a BlackJackRound now I just need to see how can i implement this in the main but the code overall is 100 times better and more organized even though it does the same thing

---
+++ src/main/java/BlackJack/BlackJackRound.java
@@ -0,0 +1,86 @@
+package BlackJack;
+
+public class BlackJackRound {
+
+    private Deck deck;
+    private Hand dealer;
+    private Hand player;
+    private boolean roundOver;
+
+    public void start(){
+
+        this.deck = new Deck();
+        this.dealer = new Hand();
+        this.player = new Hand();
+        this.roundOver = false;
+
+        player.addCard(deck.draw());
+        dealer.addCard(deck.draw());
+        player.addCard(deck.draw());
+        dealer.addCard(deck.draw());
+    }
+
+    public Card dealerUpCard(){
+        return dealer.getCards().getFirst();
+    }
+
+    public Hand getDealer(){
+        return dealer;
+    }
+
+    public Hand getPlayer(){
+        return player;
+    }
+
+    public void hit(){
+        player.addCard(deck.draw());
+        if (player.getTotal() > 21){
+            roundOver = true;
+        }
+    }
+
+    public void stand(){
+        dealerPlay();
+        roundOver = true;
+    }
+
+    public void DoubleDown(){
+        player.addCard(deck.draw());
+        dealerPlay();
+        roundOver = true;
+    }
+
+    public Result getResult(){
+        if (!roundOver){
+            if (player.isBlackJack()){
+                if (dealer.getCards().getFirst().getValue() == 10 || dealer.getCards().getFirst().getValue() == 11){
+                    if (dealer.isBlackJack()){
+                        return Result.PUSH;
+                    }
+                    return Result.PLAYER_BLACKJACK;
+                }
+            } else {
+                return Result.NOT_FINISHED;
+            }
+        }
+        if (player.getTotal() > 21){
+            return Result.PLAYER_BUST;
+        }
+        if (dealer.getTotal() > 21){
+            return Result.DEALER_BUST;
+        }
+        if (player.getTotal() > dealer.getTotal()){
+            return Result.PLAYER_WIN;
+        }
+        if (dealer.getTotal() > player.getTotal()){
+            return Result.DEALER_WIN;
+        }
+        return Result.PUSH;
+    }
+
+    private void dealerPlay() {
+        while (dealer.getTotal() < 17){
+            dealer.addCard(deck.draw());
+        }
+    }
+}
---
+++ src/main/java/BlackJack/Result.java
@@ -0,0 +1,11 @@
+package BlackJack;
+
+public enum Result {
+    PLAYER_BUST,
+    DEALER_BUST,
+    PLAYER_BLACKJACK,
+    PLAYER_WIN,
+    DEALER_WIN,
+    PUSH,
+    NOT_FINISHED
+}
